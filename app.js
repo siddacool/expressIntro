7d020049de16382b6824995d8e33ebcc7cc57a27
Siddhesh Mangela <sidthecool007@gmail.com>
2016-03-22 16:17:08 +0530

    removed old node.js code

--- app.js
+++ app.js
@@ -27,15 +27,3 @@ app.listen(1337,function(){
     console.log('yo mama 1337');
 });
 
-
-//const http = require('http');
-//
-//const hostname = '127.0.0.1';
-//const port = 1337;
-//
-//http.createServer((req, res) => {
-//  res.writeHead(200, { 'Content-Type': 'text/plain' });
-//  res.end('Hello World\n');
-//}).listen(port, hostname, () => {
-//  console.log(`Server running at http://${hostname}:${port}/`);
-//});
